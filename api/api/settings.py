ddfe3b8e93ab4e057b0aa449a34ca85298adc6a9
Leonid Zagumennov <leonzag997@gmail.com>
2023-04-07 16:30:40 +0300

    + add python-dotenv to project for secured env

--- api/api/settings.py
+++ api/api/settings.py
@@ -10,22 +10,26 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
+from distutils.util import strtobool
 from pathlib import Path
+import sys
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+load_dotenv()
+if not os.getenv('SECRET_KEY'):
+    print(f"Create .env in {BASE_DIR} and set SECRET_KEY variable", file=sys.stderr)
+    exit(1)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-22xct7&neq!#_*c@9ci9qgchfi=$&aa7nq5k^7hi_b-)@*3w8e'
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = bool(strtobool(os.getenv("DEBUG", 'True')))
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ['localhost',]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'localhost').split(',')
 
 
 # Application definition
@@ -104,9 +108,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'ru-ru'
-
-TIME_ZONE = 'Europe/Moscow'
+LANGUAGE_CODE = os.getenv('LANGUAGE_CODE', 'ru-ru')
+TIME_ZONE = os.getenv('TIME_ZONE', 'Europe/Moscow')
 
 USE_I18N = True
 
